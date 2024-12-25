3cbb73a5582c256e3debb27f32e5b435262d8fd8
Thoriz <63641587+a6cenna@users.noreply.github.com>
2024-12-25 17:28:53 +0700

    ngotot contest 173 prob c

--- codeforces/173/2043C.cpp
+++ codeforces/173/2043C.cpp
@@ -9,29 +9,68 @@ void solve() {
     {
         cin>>a[i];
     }
-    vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
-
-    for (int i = 1; i <= n; i++) {
-        for (int j = i; j <= n; j++) {
-            dp[i][j] = dp[i][j - 1] + a[j - 1];
-        }
+    int idx=0;
+    for(int i=0;i<n;i++) {
+        if(abs(a[i])!=1) idx=i;
     }
-    set<int> sums;
-    sums.insert(0);
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = 1; j <=n; j++)
-        {
-            sums.insert(dp[i][j]);
-        }
-        
+    pair<int,int> s, p, s1, s2;
+    int x=0;
+    for(int i=idx-1;i>=0;i--) {
+        x+=a[i];
+        s.first=min(s.first, x);
+        s.second=max(s.second, x);
+        s1.first=min(s1.first, x-s.second);
+        s1.second=max(s1.second, x-s.first);
     }
-    cout << sums.size() << endl;
-    for (auto sum: sums) 
+    x=0;
+    for(int i=idx+1;i<n;i++) {
+        x+=a[i];
+        p.first=min(p.first, x);
+        p.second=max(p.second, x);
+        s2.first=min(s2.first, x-p.second);
+        s2.second=max(s2.second, x-p.first);
+    }
+    set<int> sum;
+    for (int i = s1.first; i <= s1.second; i++)
     {
-        cout << sum << " ";
+        sum.insert(i);
+    }
+    for (int i = s2.first; i <= s2.second; i++)
+    {
+        sum.insert(i);
+    }
+    for(int i=a[idx]+s.first+p.first;i<=a[idx]+s.second+p.second;i++) {
+        sum.insert(i);
+    }
+    
+    cout << sum.size() << '\n';
+    for(auto a:sum) {
+        cout << a << " ";
     }
     cout << '\n';
+    // vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+
+    // for (int i = 1; i <= n; i++) {
+    //     for (int j = i; j <= n; j++) {
+    //         dp[i][j] = dp[i][j - 1] + a[j - 1];
+    //     }
+    // }
+    // set<int> sums;
+    // sums.insert(0);
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     for (int j = 1; j <=n; j++)
+    //     {
+    //         sums.insert(dp[i][j]);
+    //     }
+        
+    // }
+    // cout << sums.size() << endl;
+    // for (auto sum: sums) 
+    // {
+    //     cout << sum << " ";
+    // }
+    // cout << '\n';
 }
 
 int main() {
