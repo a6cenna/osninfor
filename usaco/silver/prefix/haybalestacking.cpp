d48e87c1276b1189f90b9fcd2de1d4693186bdf4
Thoriz <63641587+a6cenna@users.noreply.github.com>
2024-12-26 17:40:44 +0700

    hummm
    got stuck on haybale stacking

---
+++ usaco/silver/prefix/haybalestacking.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    // freopen("filename.in", "r", stdin);
+    // freopen("filename.out", "w", stdout);
+    int n,k;cin>>n>>k;
+    vector<int> pre(n+1, 0);
+    for (int i = 0; i < k; i++)
+    {
+        int a,b;cin>>a>>b;
+        
+    }
+    
+}
