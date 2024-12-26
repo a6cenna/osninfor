6c1756eb75e444e5f0235f6275cd55c8b24329f5
Thoriz <63641587+a6cenna@users.noreply.github.com>
2024-12-26 17:58:36 +0700

    finally
    cost at what

--- usaco/silver/prefix/haybalestacking.cpp
+++ usaco/silver/prefix/haybalestacking.cpp
@@ -13,7 +13,18 @@ int main() {
     for (int i = 0; i < k; i++)
     {
         int a,b;cin>>a>>b;
-        
+        a--;
+        b--;
+        pre[a]++;
+        pre[b+1]--;
     }
-    
+    int ans[n];
+    int sum=0;
+    for (int i = 0; i < n; i++)
+    {
+        sum+=pre[i];
+        ans[i]=sum;
+    }
+    sort(ans, ans+n);
+    cout << ans[n/2];
 }
